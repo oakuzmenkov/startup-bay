0f356ca1ad8194c6e8a4abc44b9e228110bbc644
oakuzmenkov <oakuzmenkov@gmail.com>
2020-05-03 01:39:39 +0300

    гугл аналитика и сспонсорские ссылки добавлены

--- .vuepress/config.js
+++ .vuepress/config.js
@@ -2,11 +2,23 @@ module.exports = {
     title: "Стартап Бухта",
     description: "Стартап Бухта — подкаст стартаперов для стартаперов",
     head: [["link", { rel: "icon", href: "/favicon.png" }]],
+    themeConfig: {
+        logo: "/favicon.png",
+        nav: [
+            { text: "Home", link: "/" },
+            { text: "JSHunt", link: "https://jshunt.by", rel: "sponsored" },
+            {
+                text: "Seller Assistant App",
+                link: "https://sellerassistant.app",
+                rel: "sponsored",
+            },
+        ],
+    },
     plugins: [
         [
             "@vuepress/google-analytics",
             {
-                // ga: "", // UA-00000000-0
+                ga: "UA-165377879-1", // UA-00000000-0
             },
         ],
     ],
